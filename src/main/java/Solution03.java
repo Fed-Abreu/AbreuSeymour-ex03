678284e6964bb07e58b234b0fd929f8d3dfe9aa6
Fed-Abreu <arielsey@hotmail.com>
2021-09-05 16:33:46 -0400

    Initial commit

---
+++ src/main/java/Solution03.java
@@ -0,0 +1,27 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+public class Solution03 {
+    /*
+    print "What is the quote?"
+    add scanner string quote = input
+    print "Who said it?"
+    Scan string name = input
+    print name + "says, " + \"quote\"
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = input.nextLine();
+        System.out.print("Who said it? ");
+        String name = input.nextLine();
+        System.out.print(name + " says, " + "\"" + quote +"\"");
+
+
+
+    }
+}
